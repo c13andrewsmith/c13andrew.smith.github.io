b2976a75b65c9e211ef991b7dc8d2bffb0b21f42
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-19 14:52:41 -0700

    Add image switching script

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'img/160px-1954_Bowman_Mickey_Mantle.jpeg') {
+      myImage.setAttribute('src','img/220px-Mickey_Mantle_-_New_York_Yankees_-_1957.jpeg');
+    } else {
+      myImage.setAttribute('src','img/220px-Mickey_Mantle_1951.jpeg');
+    }
+}
