453c7b73026385f32b0ceb970dc109492abd8aed
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-19 15:55:01 -0700

    Add image cycling

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,9 @@ myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'img/160px-1954_Bowman_Mickey_Mantle.jpeg') {
       myImage.setAttribute('src','img/220px-Mickey_Mantle_-_New_York_Yankees_-_1957.jpeg');
-    } else {
+    } else if (mySrc === 'img/220px-Mickey_Mantle_-_New_York_Yankees_-_1957.jpeg'){
       myImage.setAttribute('src','img/220px-Mickey_Mantle_1951.jpeg');
+    } else if (mySrc === 'img/220px-Mickey_Mantle_1951.jpeg'){
+      myImage.setAttribute('src','img/160px-1954_Bowman_Mickey_Mantle.jpeg');
     }
 }
